be2b4202be79ddb6997d604bd67c5f87fbb14a1f
Antonio Espinoza <a0e0bhj@walmart.com>
2025-08-04 16:29:47 -0400

    Add PATCH endpoint for partial employee updates and refactor EmployeeRestController

--- src/main/java/com/antonio/cruddemo/rest/EmployeeRestController.java
+++ src/main/java/com/antonio/cruddemo/rest/EmployeeRestController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.antonio.cruddemo.entity.Employee;
 import com.antonio.cruddemo.service.EmployeeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -21,8 +25,11 @@ public class EmployeeRestController {
 
     private EmployeeService employeeService;
 
-    public EmployeeRestController(EmployeeService employeeService) {
+    private ObjectMapper objectMapper;
+
+    public EmployeeRestController(EmployeeService employeeService, ObjectMapper objectMapper) {
         this.employeeService = employeeService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/employees")
@@ -34,11 +41,11 @@ public class EmployeeRestController {
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId) {
         Employee theEmployee = employeeService.findById(employeeId);
-        
+
         if (theEmployee == null) {
             throw new RuntimeException("Employee id not found - " + employeeId);
         }
-        
+
         return theEmployee;
     }
 
@@ -48,9 +55,9 @@ public class EmployeeRestController {
         // also just in case they pass an id in JSON ... set id to 0
         // this is to force a save of new item ... instead of update
         theEmployee.setId(0);
-        
+
         Employee dbEmployee = employeeService.save(theEmployee);
-        
+
         return dbEmployee;
     }
 
@@ -58,22 +65,56 @@ public class EmployeeRestController {
     @PutMapping("/employees")
     public Employee updateEmployee(@RequestBody Employee theEmployee) {
         Employee dbEmployee = employeeService.save(theEmployee);
-        
+
         return dbEmployee;
+    }
+
+    // add mapping for PATCH /employees/{employeeId} - patch employee ... partial
+    // update
+    @PatchMapping("/employees/{employeeId}")
+    public Employee patchEmployee(@PathVariable int employeeId, @RequestBody Map<String, Object> patchPayload) {
+        // first get the existing employee from database
+        Employee existingEmployee = employeeService.findById(employeeId);
+
+        // throw exception if null
+        if (existingEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+        // throw exception if request body contains "id" key
+        if (patchPayload.containsKey("id")) {
+            throw new RuntimeException("Employee id not allowed in request body - " + employeeId);
+        }
+
+        Employee patchedEmployee = apply(patchPayload, existingEmployee);
+
+        Employee dbEmployee = employeeService.save(patchedEmployee);
+
+        return dbEmployee;
+    }
+
+    private Employee apply(Map<String, Object> patchPayload, Employee existingEmployee) {
+        // Convert employee ojbect to a JSON object node
+        ObjectNode employeeNode = objectMapper.convertValue(existingEmployee, ObjectNode.class);
+        // Convert the patchPayload map to a JSON object node
+        ObjectNode patchNode = objectMapper.convertValue(patchPayload, ObjectNode.class);
+        // Merge the patchNode into employeeNode
+        employeeNode.setAll(patchNode);
+
+        return objectMapper.convertValue(employeeNode, Employee.class);
     }
 
     // add mapping for DELETE /employees/{employeeId} - delete employee
     @DeleteMapping("/employees/{employeeId}")
     public String deleteEmployee(@PathVariable int employeeId) {
         Employee tempEmployee = employeeService.findById(employeeId);
-        
+
         // throw exception if null
         if (tempEmployee == null) {
             throw new RuntimeException("Employee id not found - " + employeeId);
         }
-        
+
         employeeService.deleteById(employeeId);
-        
+
         return "Deleted employee id - " + employeeId;
     }
 
