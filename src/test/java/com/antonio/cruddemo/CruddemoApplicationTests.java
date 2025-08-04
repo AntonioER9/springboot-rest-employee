ac5e050709da24173fdb2411f698802bd905468e
Antonio Espinoza <a0e0bhj@walmart.com>
2025-08-04 15:49:35 -0400

    first commit

---
+++ src/test/java/com/antonio/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonio.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
