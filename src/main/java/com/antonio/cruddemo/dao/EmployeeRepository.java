602f0db972777fe4f1684d169d31928f847a96c7
Antonio Espinoza <a0e0bhj@walmart.com>
2025-08-05 17:58:23 -0400

    Add Springdoc OpenAPI dependency and update application properties for default page size

--- src/main/java/com/antonio/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/antonio/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,13 @@
 package com.antonio.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.antonio.cruddemo.entity.Employee;
 
+// Uncomment the following line to customize the repository path
+// If you want to change the default path from "employees" to "members", uncomment the line below
+// @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
